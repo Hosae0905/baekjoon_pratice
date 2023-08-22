1d35d262a02ed1b3d278b69a9583626938750460
didgh <didghtls11@gmail.com>
2023-08-23 00:52:52 +0900

    23.08.23 스택 & 큐 & 덱 알고리즘 풀이

---
+++ src/Sliver/Silver3_12789.java
@@ -0,0 +1,49 @@
+package Sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Silver3_12789 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Stack<Integer> standbyLine = new Stack<>();
+        Queue<Integer> currentLine = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            currentLine.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int start = 1;
+
+        while (!currentLine.isEmpty()) {
+            if (currentLine.peek() == start) {
+                currentLine.poll();
+                start++;
+            } else if (!standbyLine.isEmpty() && standbyLine.peek() == start) {
+                standbyLine.pop();
+                start++;
+            } else {
+                standbyLine.push(currentLine.poll());
+            }
+        }
+
+        while (!standbyLine.isEmpty()) {
+            if (standbyLine.peek() == start) {
+                standbyLine.pop();
+                start++;
+            } else {
+                System.out.println("Sad");
+                return;
+            }
+        }
+        System.out.println("Nice");
+    }
+}
---
+++ src/Sliver/Silver3_2346.java
@@ -0,0 +1,51 @@
+package Sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Silver3_2346 {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Deque<int[]> deque = new ArrayDeque<>();
+
+        int[] K = new int[N];
+        for (int i = 0; i < N; i++) {
+            K[i] = Integer.parseInt(st.nextToken());
+        }
+
+        sb.append("1 ");
+        int T = K[0];
+
+        for (int i = 1; i < N; i++) {
+            deque.add(new int[] {(i+1), K[i]});
+        }
+
+        while (!deque.isEmpty()) {
+            if (T > 0) {
+                for (int i = 1; i < T; i++) {
+                    deque.add(deque.poll());
+                }
+                int[] next = deque.poll();
+                T = next[1];
+                sb.append(next[0] + " ");
+            } else {
+                for (int i = 1; i <- T; i++) {
+                    deque.addFirst(deque.pollLast());
+                }
+
+                int[] next = deque.pollLast();
+                T = next[1];
+                sb.append(next[0] + " ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
