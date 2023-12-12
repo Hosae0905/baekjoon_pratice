0b1b91f411d5bd4e23f685e69e7fa7f639a07939
양호신 <didghtls11@gmail.com>
2023-12-12 17:25:22 +0900

    [level 1] Title: 체육복, Time: 0.54 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int count = n - lost.length;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    count++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j] - 1 || lost[i] == reserve[j] + 1) {
+                    count++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+}
