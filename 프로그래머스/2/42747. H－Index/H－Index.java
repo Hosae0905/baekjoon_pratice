cbdf0e1b20caeb74d08cddb1f38440ff65afba22
양호신 <didghtls11@gmail.com>
2023-12-13 22:42:56 +0900

    [level 2] Title: H-Index, Time: 0.27 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < citations.length; i++) {
+            list.add(citations[i]);
+        }
+
+        list.sort(Collections.reverseOrder());
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(0) == 0) {
+                return 0;
+            } else if (list.get(i) <= i) {
+                break;
+            } else {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
