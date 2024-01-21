d1442c823ff861e826878b56920f07efaec45df5
양호신 <didghtls11@gmail.com>
2024-01-21 22:24:27 +0900

    [level 2] Title: 올바른 괄호, Time: 21.18 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -6,14 +6,16 @@ class Solution {
 
         char[] charArray = s.toCharArray();
 
-        for (int i = 0; i < charArray.length; i++) {
-            if (charArray[i] == '(') {
-                stack.push('(');
-            } else if (charArray[i] == ')') {
-                if (stack.isEmpty()) return false;
+        for (char c : charArray) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else if (stack.peek() == '(' && c == ')') {
                 stack.pop();
+            } else {
+                stack.push(c);
             }
         }
+
         return stack.isEmpty();
     }
 }
