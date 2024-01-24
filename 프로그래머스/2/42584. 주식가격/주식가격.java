d2b0a7afdddaa83faf791f6b17aa043beca183a5
양호신 <didghtls11@gmail.com>
2024-01-24 23:40:31 +0900

    [level 2] Title: 주식가격, Time: 11.60 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,26 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        Queue<Integer> queue = new LinkedList<>();
+        int[] answer = new int[prices.length];
 
         for (int i = 0; i < prices.length; i++) {
-            queue.add(prices[i]);
-        }
-
-        int[] answer = new int[prices.length];
-        int index = 0;
-        while (!queue.isEmpty()) {
-            int currentPrice = queue.poll();
-            for (int i = (prices.length - queue.size()); i < prices.length; i++) {
-                if (currentPrice > prices[i]) {
-                    answer[index]++;
+            int value = prices[i];
+            for (int j = i + 1; j < prices.length; j++) {
+                if (value > prices[j]) {
+                    answer[i]++;
                     break;
-                } else if (currentPrice <= prices[i]) {
-                    answer[index]++;
+                } else {
+                    answer[i]++;
                 }
             }
-            index++;
         }
+        
         return answer;
     }
 }
