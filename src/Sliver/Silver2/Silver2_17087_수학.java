b4d123e360bfa6ca66f2d8409692e484fa1b366e
hosin <didghtls11@gmail.com>
2024-05-10 16:21:43 +0900

    2024-05-10 알고리즘 공부

---
+++ src/Sliver/Silver2/Silver2_17087_수학.java
@@ -0,0 +1,35 @@
+package Sliver.Silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Silver2_17087_수학 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.abs(S - Integer.parseInt(st.nextToken()));
+        }
+
+        int value = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            value = gcp(value, arr[i]);
+        }
+
+        System.out.println(value);
+    }
+
+    public static int gcp(int a, int b) {
+        if (b == 0) return a;
+        return gcp(b, a % b);
+    }
+}
---
+++ src/Sliver/Silver2/Sliver2_17103_수학.java
@@ -0,0 +1,55 @@
+package Sliver.Silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sliver2_17103_수학 {
+
+    static int[] prime = new int[1000001];
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        makePrime();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 2; j <= arr[i] / 2; j++) {
+                if (prime[j] != 0 && prime[arr[i] - j] != 0) {
+                    System.out.println(j + ", " + (arr[i] - j));
+                    count++;
+                }
+            }
+            sb.append(count).append("\n");
+            count = 0;
+        }
+
+
+        System.out.println(sb);
+    }
+
+    public static void makePrime() {
+        for (int i = 1; i < prime.length; i++) {
+            prime[i] = i;
+        }
+
+        for (int i = 0; i < Math.sqrt(prime.length); i++) {
+            if (prime[i] == 0) continue;
+            if (prime[i] == 1) {
+                prime[i] = 0;
+                continue;
+            }
+            for (int j = i * 2; j < prime.length; j+=i) {
+                prime[j] = 0;
+            }
+        }
+    }
+}
