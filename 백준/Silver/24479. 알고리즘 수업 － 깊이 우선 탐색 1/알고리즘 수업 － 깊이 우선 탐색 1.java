e25f8eaa00f8b481587e073e7629b38a6b491fab
양호신 <didghtls11@gmail.com>
2024-05-15 23:46:06 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1884 ms, Memory: 96892 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N, M, R;
+    static boolean[] visited;
+    static ArrayList<Integer>[] graph;
+    static int[] answer;
+    static int order;
+
+
+    public static void dfs(int start) {
+        visited[start] = true;
+        answer[start] = order;
+        order++;
+
+        for (int i = 0; i < graph[start].size(); i++) {
+            int value = graph[start].get(i);
+            if (visited[value] == false) {
+                dfs(value);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        graph = new ArrayList[N + 1];
+        answer = new int[N + 1];
+        order = 1;
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x].add(y);
+            graph[y].add(x);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        dfs(R);
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(answer[i]);
+        }
+    }
+}
