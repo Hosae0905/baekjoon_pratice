f79467b3619228766561e5a424049d9ed9f90b72
양호신 <didghtls11@gmail.com>
2024-05-20 15:22:10 +0900

    [Gold V] Title: 탑, Time: 832 ms, Memory: 101660 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         Stack<int[]> stack = new Stack<>();
@@ -12,16 +13,18 @@ public class Main {
             int top = Integer.parseInt(st.nextToken());
             while (!stack.isEmpty()) {
                 if (stack.peek()[1] >= top) {
-                    System.out.print(stack.peek()[0] + " ");
+                    sb.append(stack.peek()[0] + " ");
                     break;
                 }
                 stack.pop();
             }
 
             if (stack.isEmpty()) {
-                System.out.print("0 ");
+                sb.append(0 + " ");
             }
             stack.push(new int[]{i, top});
         }
+
+        System.out.println(sb);
     }
 }
