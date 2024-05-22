d34ebc76c38f79b2b6fb46fa8e036b876d447444
hosin <didghtls11@gmail.com>
2024-05-22 14:45:32 +0900

    2024-05-22 알고리즘 공부

---
+++ src/Sliver/Silver1/Silver1_11286_자료구조.java
@@ -0,0 +1,44 @@
+package Sliver.Silver1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Silver1_11286_자료구조 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) > Math.abs(o2)) {
+                    return Math.abs(o1) - Math.abs(o2);
+                } else if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                } else {
+                    return -1;
+                }
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            int M = Integer.parseInt(br.readLine());
+
+            if (M == 0) {
+                if (queue.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.add(M);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
