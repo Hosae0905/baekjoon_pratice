dd565fa20c8c142c4bda38d01f88ff4cf2058f6f
양호신 <didghtls11@gmail.com>
2024-06-19 11:36:27 +0900

    [Silver III] Title: 순열 사이클, Time: 1788 ms, Memory: 301764 KB -BaekjoonHub

---
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static boolean[] visited;
+    static boolean[][] graph;
+    static int answer;
+
+    public static void dfs(int value) {
+        visited[value] = true;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && graph[value][i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        while (T-- > 0) {
+            N = Integer.parseInt(br.readLine());
+            answer = 0;
+            visited = new boolean[N + 1];
+            graph = new boolean[N + 1][N + 1];
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 1; i <= N; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                graph[i][num] = true;
+                graph[num][i] = true;
+            }
+
+            for (int i = 1; i <= N; i++) {
+                if (!visited[i]) {
+                    dfs(i);
+                    answer++;
+                }
+            }
+
+            bw.write(String.valueOf(answer));
+            bw.newLine();
+        }
+
+        bw.close();
+        br.close();
+    }
+}
