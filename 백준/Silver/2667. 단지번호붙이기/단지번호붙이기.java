23d155ddae99390b2ca7c020ebd18f4c414f32a7
양호신 <didghtls11@gmail.com>
2024-06-24 11:02:40 +0900

    [Silver I] Title: 단지번호붙이기, Time: 160 ms, Memory: 15976 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX = 25 + 10;
+    static int N, count;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dirY = {-1, 1, 0, 0};
+    static int[] dirX = {0, 0, -1, 1};
+
+    public static void dfs(int y, int x) {
+        visited[y][x] = true;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < 4; j++) {
+                int newY = y + dirY[j];
+                int newX = x + dirX[j];
+
+                if (map[newY][newX] == 1 && !visited[newY][newX]) {
+                    count++;
+                    dfs(newY, newX);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[MAX][MAX];
+        visited = new boolean[MAX][MAX];
+        count = 1;
+
+        for (int i = 1; i <= N; i++) {
+            String str = br.readLine();
+            for (int j = 1; j <= N; j++) {
+                map[i][j] = str.charAt(j - 1) - '0';
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    dfs(i, j);
+                    list.add(count);
+                    count = 1;
+                }
+            }
+        }
+
+        Collections.sort(list);
+
+        bw.write(list.size() + "\n");
+        for (Integer value : list) {
+            bw.write(value + "\n");
+        }
+        bw.close();
+        br.close();
+    }
+}
