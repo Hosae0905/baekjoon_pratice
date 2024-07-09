bb548ff98c45531d3f5171cf2970900a0bd3426b
hosin <didghtls11@gmail.com>
2024-07-09 14:14:08 +0900

    2024-07-09 알고리즘 공부

---
+++ src/Bronze/Bronze3/Bronze3_2442_별찍기5.java
@@ -0,0 +1,29 @@
+package Bronze.Bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bronze3_2442_별찍기5 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = i; j < N; j++) {
+                sb.append(" ");
+            }
+
+            for (int j = 0; j < i * 2 - 1; j++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
