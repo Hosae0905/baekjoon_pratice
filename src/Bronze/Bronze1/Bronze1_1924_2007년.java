569db18f6ddb5cbd9fe8052efa57c7ea2251a98d
hosin <didghtls11@gmail.com>
2024-07-11 11:09:52 +0900

    2024-07-11 알고리즘 공부

---
+++ src/Bronze/Bronze1/Bronze1_1924_2007년.java
@@ -0,0 +1,40 @@
+package Bronze.Bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bronze1_1924_2007년 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
+        int totalDay = -1;
+
+        int[] arr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] days = new String[365];
+        String[] str = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+
+        for (int i = 0; i < days.length; i += 7) {
+            for (int j = 0; j < str.length; j++) {
+                if (i + j >= 365) {
+                    break;
+                }
+                days[i + j] = str[j];
+            }
+        }
+
+        if (month == 1) {
+            System.out.println(days[totalDay += day]);
+        } else {
+            for (int i = 0; i < month - 1; i++) {
+                totalDay += arr[i];
+            }
+
+            System.out.println(days[totalDay += day]);
+        }
+
+    }
+}
