8f497d3a68e737260aa3de82c62de35498675d71
양호신 <didghtls11@gmail.com>
2024-10-29 22:25:34 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,14 +1,29 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+/**
+ * 백준 1427번
+ * 간단한 내림차순 정렬 문제이다.
+ * 정렬하려고 하는 수 N이 주어지는데 10억보다 작거나 같은 자연수이다.
+ *
+ * 입력값을 문자열로 받아서 일반 문자로 나눈 뒤 정렬을 진행한다.
+ * 문자열로 정렬을 진행하면 10억이라도 10개의 문자로 나눌 수 있기 때문에 시간 복잡도를 크게 고려하지 않아도 된다.
+ */
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char[] N = sc.next().toCharArray();
-        Arrays.sort(N);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();     // 입력값을 저장할 문자열 변수
 
-        for (int i = N.length-1; i >= 0; i--) {
-            System.out.print(N[i]);
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i) - '0');
+        }
+
+        Collections.sort(list);
+
+        for(int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i));
         }
     }
 }
