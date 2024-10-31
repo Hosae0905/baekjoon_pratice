d574a84d4acd5fffdfbd02c6319596afb5a11aee
양호신 <didghtls11@gmail.com>
2024-10-31 15:29:56 +0900

    [Silver V] Title: 카약, Time: 104 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/2890. 카약/카약.java
+++ 백준/Silver/2890. 카약/카약.java
@@ -19,32 +19,41 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());       // 행
+        int c = Integer.parseInt(st.nextToken());       // 열
 
-        ArrayList<int[]> list = new ArrayList<>();
+        ArrayList<int[]> list = new ArrayList<>();      // 카약 번호와 점수를 담을 리스트
 
+        // 한 라인씩 돌면서 카약 번호와 점수를 계산한다.
         for (int i = 0; i < r; i++) {
-            String str = br.readLine();
-            int start = -1;
-            int num = -1;
-            int diff = 0;
+            String str = br.readLine();     // 라인 하나
+            int start = -1;      // 출발지점 초기화
+            int num = -1;        // 카약 번호
+            int score = 0;       // 점수
+
+            // 하나의 라인에서 글자 하나씩 뽑아 점수를 계산한다.
             for (int j = 0; j < c; j++) {
-                char x = str.charAt(j);
+                char x = str.charAt(j);     // 라인에서 글자 하나 뽑기
+
+                /*
+                * 만약 뽑은 글자가 숫자라면 시작점과 카약 번호를 구한다.
+                * 그렇지 않고 만약 뽑은 글자가 'F'라면 종료지점이기 때문에 최종 점수를 구한다.
+                * */
                 if (Character.isDigit(x)) {
                     start = j;
                     num = x - '0';
                 } else if (x == 'F') {
-                    diff = c - 1 - start;
+                    score = c - 1 - start;
                 }
             }
 
+            // 만약 카약 번호가 -1이 아니라면 리스트에 해당 카약 번호와 점수를 저장한다.
             if (num != -1) {
-                list.add(new int[]{num, diff});
+                list.add(new int[]{num, score});
             }
         }
 
+        // 점수를 기준으로 오름차순 정렬을 진행한다.
         list.sort(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -52,10 +61,15 @@ public class Main {
             }
         });
 
-        int result[] = new int[10];
-        int diff = 0;
-        int index = 0;
+        int result[] = new int[10];     // 결과를 담을 배열
+        int diff = 0;                   // 앞 팀의 결승선과 떨어진 거리
+        int index = 0;                  // 등수
         for (int i = 0; i < 9; i++) {
+            /*
+            * 만약 앞 팀의 결승선과 떨어진 거리가 다르다면 등수가 달라진다.
+            * 정답 배열에 카약 번호의 위치에서 등수를 저장한다.
+            * 그렇지 않다면(앞 팀의 결승선과 떨어진 거리가 같다면) 같은 등수이기 때문에 같은 등수로 저장한다.
+            * */
             if (diff != list.get(i)[1]) {
                 result[list.get(i)[0]] = ++index;
                 diff = list.get(i)[1];
