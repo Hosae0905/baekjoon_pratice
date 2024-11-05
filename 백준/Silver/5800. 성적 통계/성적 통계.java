e95eda1b54d9ce2bc496818f2b26a8d5f19025a8
양호신 <didghtls11@gmail.com>
2024-11-05 23:22:15 +0900

    [Silver V] Title: 성적 통계, Time: 116 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/5800. 성적 통계/성적 통계.java
+++ 백준/Silver/5800. 성적 통계/성적 통계.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 /**
  * 각 반의 수학 시험 성적의 통계를 낸다.
  * 각 반의 학생들의 수학 성적이 주어졌을 때 최대 점수, 최소 점수, 점수 차이를 구하는 프로그램을 작성해라.
@@ -22,41 +26,36 @@ public class Main {
         for(int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());       // 각 반마다 학생 수
-            LinkedList<Integer> list = new LinkedList<>();  // 각 반의 수학 성적을 저장할 연결 리스트
+            int[] arr = new int[n];
 
             sb.append("Class ").append(i + 1).append("\n");     // 몇 반의 성적을 출력할지 저장한다.
 
             // 연결 리스트에 수학 성적을 저장한다.
             for(int j = 0; j < n; j++) {
-                list.add(Integer.parseInt(st.nextToken()));
+                arr[j] = Integer.parseInt(st.nextToken());
             }
 
-            // 내림차순으로 정렬한다.
-            Collections.sort(list, new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return o2 - o1;
-                }
-            });
+            // 오름차순으로 정렬한다.
+            Arrays.sort(arr);
 
-            int max = list.getFirst();      // 가장 높은 점수
-            int min = list.getLast();       // 가장 낮은 점수
+            int max = arr[n - 1];           // 가장 높은 점수
+            int min = arr[0];               // 가장 낮은 점수
             int gap = 0;                    // 인접한 점수 차이를 저장할 변수
 
             // 각 반의 총 학생 수 만큼 인접한 성적 차이를 저장한다.
-            for(int j = 0; j < list.size() - 1; j++) {
+            for(int j = 0; j < arr.length - 1; j++) {
 
                 /*
                 * 만약 학생의 성적이 gap 보다 작다면 현재 gap이 가장 높은 인접한 점수인 것을 알 수 있다.
                 * 예를 들어 첫 번째 예저를 보면 76과 30의 차이는 46이다.
                 * 만약 다음에 숫자가 0이 오더라도 그 차이는 46이 되기 때문에 결국 점수가 gap보다 작다면 해당 gap 최댓값인 것을 알 수 있다.
                 * */
-                if (list.get(j) < gap) {
+                if (arr[j] < gap) {
                     break;
                 }
 
                 // 만약 현재 인접한 점수차와 앞선 학생의 수학 점수와 현재 학생의 수학 점수 차이를 비교하여 더 크다면 gap을 변경해준다.
-                gap = Math.max(gap, list.get(j) - list.get(j + 1));
+                gap = Math.max(gap, arr[j + 1] - arr[j]);
             }
 
             // 최종적으로 가장 높은 점수와 가장 낮은 점수 그리고 가장 큰 인접한 점수 차이를 sb에 저장한다.
